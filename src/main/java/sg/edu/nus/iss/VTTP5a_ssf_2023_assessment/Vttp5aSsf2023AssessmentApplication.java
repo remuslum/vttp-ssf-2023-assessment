1484a473feff1ea0d028f99b05de2a704033fc40
remuslum <57741494+remuslum@users.noreply.github.com>
2024-12-12 09:56:50 +0800

    First commit

---
+++ src/main/java/sg/edu/nus/iss/VTTP5a_ssf_2023_assessment/Vttp5aSsf2023AssessmentApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.VTTP5a_ssf_2023_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsf2023AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsf2023AssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/VTTP5a_ssf_2023_assessment/Vttp5aSsf2023AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.VTTP5a_ssf_2023_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsf2023AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
